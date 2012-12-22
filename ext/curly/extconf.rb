ddc66185131c16d7314ab3ce5a73783e706d999a
John Ledbetter <john.ledbetter@gmail.com>
2012-12-22 11:04:11 -0500

    whoops -- was building the shared library in the wrong place.

--- ext/curly/extconf.rb
+++ ext/curly/extconf.rb
@@ -7,7 +7,7 @@ find_library('curl', 'curl_easy_init',
   '/usr/lib'
 )
 
-create_makefile('curly')
+create_makefile('curly/curly')
 
 # This hack allows flymake-mode to run syntax checking.
 File.open(File.join(File.dirname(__FILE__), "Makefile"), "a") do |f|
